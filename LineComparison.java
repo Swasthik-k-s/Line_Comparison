1b9868492d88ab5aaabdf3e97ac7052b49aca19b
Swasthik <swasthik.ks22@gmail.com>
2021-08-23 15:57:23 +0530

    UC1 - Length of the line code is added

--- LineComparison.java
+++ LineComparison.java
@@ -1,10 +1,24 @@
 package com.linecomparison;
 
-public class LineComparison {
+import java.util.*;
+
+public class UC1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to Employee Wage Computation Program");
+		int x1,y1,x2,y2;
+		double length;
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Welcome to Line Comparison Computation Program");
+	    System.out.println("Enter the coordinates of Line in the form of (x1,y1) and (x2,y2)");
+	    x1=reader.nextInt();
+	    y1=reader.nextInt();
+	    x2=reader.nextInt();
+	    y2=reader.nextInt();
+		length = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+	    System.out.println("The length of the line is:"+String.format("%.4f",length));
+		
 	}
 
 }
+
